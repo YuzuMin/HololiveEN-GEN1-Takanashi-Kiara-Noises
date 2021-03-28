0b2e4b66a96f37f4faa9fbb1beb03336dd1f94f9
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-03-28 20:02:19 +0800

    Update ExampleInstrumentedTest.java

--- Stable/KiaraNoises/app/src/androidTest/java/com/yuzumin/kiaranoise/ExampleInstrumentedTest.java
+++ Stable/KiaraNoises/app/src/androidTest/java/com/yuzumin/kiaranoise/ExampleInstrumentedTest.java
@@ -21,6 +21,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.yuzumin.kiaranoise", appContext.getPackageName());
+
+        assertEquals("com.learntodroid.simplealarmclock", appContext.getPackageName());
     }
 }
